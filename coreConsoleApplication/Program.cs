57d2bea006afdf86a59ddb232e7ca0022cd2eef7
saugusth <54689875+saugusth@users.noreply.github.com>
2023-07-25 12:45:29 -0500

    varables and datatype

--- coreConsoleApplication/Program.cs
+++ coreConsoleApplication/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Lets Learn C# Programing");
+
+string name = "King Kochar";
+int age = 23;
+float discount = 1000.50F;
+double contactNumber = 1234567890;
+char gender = 'M';
+Boolean isValid = true;
+
+Console.WriteLine("Name:" + name);
+Console.WriteLine("Age:" + age);
+Console.WriteLine("Discount:" + discount);
+Console.WriteLine("Contact Number:" + contactNumber);
+Console.WriteLine("Gender:" + gender);
+Console.WriteLine("Is Valid:" + isValid);
+
