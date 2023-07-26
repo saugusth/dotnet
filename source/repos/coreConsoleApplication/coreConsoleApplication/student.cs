07010eeca0752f25a1876394ae577e8a61efe4c2
saugusth <54689875+saugusth@users.noreply.github.com>
2023-07-26 17:27:52 -0500

    class and object

---
+++ source/repos/coreConsoleApplication/coreConsoleApplication/student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coreConsoleApplication
+{
+    internal class student
+    {
+        int studentId;
+        string StudentName;
+        public void acceptDetails()
+        {
+            Console.WriteLine("Enter Student id:");
+            studentId = int.Parse(Console.ReadLine());
+            Console.Write("Enter Student Name: ");
+            StudentName = Console.ReadLine();
+        }
+        public void displayDetails()
+        {
+            Console.WriteLine("StudentId:" + studentId);
+            Console.WriteLine("Student Name:" + StudentName);
+        }
+
+    }
+}
