61a044b1ceed45c11bc25ab813afcc2ab8c52599
saugusth <54689875+saugusth@users.noreply.github.com>
2023-07-26 17:21:43 -0500

    strings

--- coreConsoleApplication/Program.cs
+++ coreConsoleApplication/Program.cs
@@ -22,18 +22,25 @@ int [] marks = new int[5] {45, 56, 65, 67, 78};
 // marks[3] = 60;
 // marks[4] = 40;
 
-foreach (int mark in marks)
-    Console.WriteLine(mark);
+// foreach (int mark in marks)
+//     Console.WriteLine(mark);
 
-int[,]multiArray = new int [3,4]{{1,2,3,4}, {1,2,3,4}, {1,2,3,4}};
+// int[,]multiArray = new int [3,4]{{1,2,3,4}, {1,2,3,4}, {1,2,3,4}};
 
-for (int i = 0; i < 2; i++){
-    for (int j = 0; j<=3; j++){
-        Console.Write(multiArray[i,j] + "\t");
-    }
-    Console.WriteLine();
-}
+// for (int i = 0; i < 2; i++){
+//     for (int j = 0; j<=3; j++){
+//         Console.Write(multiArray[i,j] + "\t");
+//     }
+//     Console.WriteLine();
+// }
 
-int [][]aggedArray = new int[2][];
-aggedArray[0] = new int[2];
-aggedArray[1] = new int [3];
+// int [][]aggedArray = new int[2][];
+// aggedArray[0] = new int[2];
+// aggedArray[1] = new int [3];
+
+string str1 = "Hello World";
+string str2 = "C# Programing";
+Console.WriteLine(str1.Length);
+string str3 = string.Concat(str1,str2);
+Console.WriteLine(str3);
+Console.WriteLine(str1.Equals(str2));
